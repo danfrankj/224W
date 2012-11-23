7a432e66e38a9ef783458b14e313e19d43be09f6
Sylvon <Sylvon@Sylvon-PC.(none)>
2012-11-23 15:35:44 -0800

    Change some parameters

--- sampling/C++/RandomWalk.cpp
+++ sampling/C++/RandomWalk.cpp
@@ -10,11 +10,12 @@ PUNGraph RandomWalk::walk(double percent) {
 	TIntV all_ids;
 	graph->GetNIdV(all_ids);
 	target_node_num = ceil(node_num * percent);
-  //cout << "Sample Target: " << target_node_num << endl;
+  cout << "Sample Target: " << target_node_num << endl;
   sample_g = TUNGraph::New();
   while (sample_g->GetNodes() < target_node_num) {
     int istart = all_ids[rand()%node_num];
     //cout << "Start from node " << istart << endl;
+	cout<<"Sampled graph size(target="<<target_node_num<<"): "<<sample_g->GetNodes()<<endl;
     start_journey(istart, all_ids);
   }
 	return sample_g;
@@ -23,11 +24,11 @@ PUNGraph RandomWalk::walk(double percent) {
 RandomWalk::RandomWalk(PUNGraph g, int cp, int minc) {
 	graph = g;
 	check_period = cp;
-	minc = min_inc;
+	min_inc = minc;
 }
 
 void RandomWalk::start_journey(int istart, const TIntV &all_node_ids) {
-	double jumpback_prob = 0.0015;
+	double jumpback_prob = 0.15;
 	int last_check_sz = sample_g->GetNodes();
 	TUNGraph::TNodeI currentI = graph->GetNI(istart);
 	int srcId = istart;
@@ -55,5 +56,8 @@ void RandomWalk::start_journey(int istart, const TIntV &all_node_ids) {
 			if (sample_g->GetNodes() >= target_node_num)
 				break;
 		}
+		if (sample_g->GetNodes() - last_check_sz < min_inc)
+			break;
+		last_check_sz = sample_g->GetNodes();
 	}
 }
--- sampling/C++/main.cpp
+++ sampling/C++/main.cpp
@@ -10,22 +10,26 @@ void ensure_dir(char* path) {
 
 int main(int argc, const char* argv[]) {
 	//char *graph_name = "C:/Users/Sylvon/CS224W/project/Wiki-Vote.txt";
-	char *graph_name = "C:/Users/Sylvon/CS224W/project/com-dblp.ungraph.txt";
+	//char *graph_name = "C:/Users/Sylvon/CS224W/project/com-dblp.ungraph.txt";
+	char *graph_name = "C:/Users/Sylvon/CS224W/project/Email-Enron.txt";
 	PUNGraph g = TSnap::LoadEdgeList<PUNGraph>(graph_name, 0, 1);
 	int sample_iters = 100;
-	int sample_percents[] = {1, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50,
-							 55, 60, 65, 70, 75, 80, 85, 90, 95};
+	//int sample_percents[] = {1, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50,
+	//						 55, 60, 65, 70, 75, 80, 85, 90, 95};
+	int sample_percents[] = {95};
+	//int sample_percents[] = {90, 95};
 	char file_path[1024];
 	char percent_dir[1024];
 	char *output_dir = "samples/";
 	RandomWalk rw(g, 500, 500*0.3);
 	time_t start, end;
-	for (int p = 0; p < sizeof(sample_percents); p++) {
+	for (int p = 0; p < sizeof(sample_percents)/sizeof(int); p++) {
 		time(&start);
 		std::cout << "Sampling " << sample_percents[p] << " percent of the graph" << std::endl;
 		sprintf(percent_dir, "%s%d/", output_dir, sample_percents[p]);
 		ensure_dir(percent_dir);
 		for (int i = 0; i < sample_iters; i++) {
+			std::cout<<"Iteration "<<i<<std::endl;
 			sprintf(file_path, "%s%d", percent_dir, i);
 			PUNGraph sample_g = rw.walk(sample_percents[p]/100.);
 			TSnap::SaveEdgeList<PUNGraph>(sample_g, file_path, "");
