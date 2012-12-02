5dfa445a26a424e3d6cfff2d78c9fb3f59643aa3
Dan Frank <danfrankj@gmail.com>
2012-12-02 12:30:49 -0800

    added variance plot function

--- plot.py
+++ plot.py
@@ -1,6 +1,6 @@
 
 import scipy
-import scipy.stats
+import scipy.stats as stats
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
@@ -69,14 +69,37 @@ def spread_plot(y, x=None, resol=10, color='blue', axes=None):
     return fig
 
 def dstat(p, q):
-  return np.max(np.abs(np.cumsum(p) - np.cumsum(q))) 
+  return np.max(np.abs(np.cumsum(p) - np.cumsum(q)))
+
+def variance_plot(arr3d):
+
+    if not np.all(arr3d[0,-1,:] == arr3d[1,-1,:]):
+        raise Exception("don't know where truth is in arr3d")
+    truth = arr3d[0,-1,:]
+
+    dstat_arr = np.apply_along_axis(dstat, 2, arr3d, truth)
+    def bootstrap_variance(arr1d, B=1000):
+        return np.array([np.var(arr1d[np.random.random_integers(0, arr1d.size-1, arr1d.size)])
+                for i in xrange(B)])
+
+
+    vars = np.apply_along_axis(bootstrap_variance, 0, dstat_arr)
+    spread_plot(vars)
+
+
 
 ### example
 dd_arr = np.fromfile('dd_mx', sep = ' ').reshape((100, 11, 1384))
 spread_plot(np.apply_along_axis(lambda x: np.sum(np.arange(1384) * x),
-				2, dd_arr)) 
+				2, dd_arr))
+plt.title("average degree")
 plt.show()
 
 spread_plot(np.apply_along_axis(dstat, 2, dd_arr, dd_arr[0,10,:]))
+plt.title('dstat_spread')
+plt.show()
+
+variance_plot(dd_arr)
+plt.title("variance of dstat")
 plt.show()
 
